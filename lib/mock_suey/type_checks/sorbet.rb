c7f6159f87389cd7ce8b3f71de95ee9ee055c277
Vitalii Iurev <vitalii@iurev.space>
2023-03-11 15:16:18 +0300

    TypeChecks::Sorbet refactoring

--- lib/mock_suey/type_checks/sorbet.rb
+++ lib/mock_suey/type_checks/sorbet.rb
@@ -20,39 +20,71 @@ module MockSuey
       end
 
       def typecheck!(method_call, raise_on_missing: false)
+        original_method_sig = get_original_method_sig(method_call)
+        return sig_is_missing(raise_on_missing) unless original_method_sig
+
+        unbound_mocked_method = get_unbound_mocked_method(method_call)
+        override_mocked_method_to_avoid_recursion(method_call)
+
+        validate_call!(
+          instance: method_call.mocked_obj,
+          original_method: unbound_mocked_method,
+          method_sig: original_method_sig,
+          args: method_call.arguments,
+          blk: method_call.block
+        )
+      end
+
+      private
+
+      def validate_call!(instance:, original_method:, method_sig:, args:, blk:)
+        T::Private::Methods::CallValidation.validate_call(
+          instance,
+          original_method,
+          method_sig,
+          args,
+          blk
+        )
+      end
+
+      def get_unbound_mocked_method(method_call)
         method_name = method_call.method_name
         mocked_obj = method_call.mocked_obj
+
         is_singleton = method_call.receiver_class.singleton_class?
-        is_a_class = mocked_obj.is_a? Class
-        unbound_mocked_method = if is_singleton
+        if is_singleton
           mocked_obj.instance_method(method_name)
         else
           mocked_obj.method(method_name).unbind
         end
-        args = method_call.arguments
+      end
 
-        unbound_original_method = if is_a_class
-          mocked_obj.method(method_name)
+      def get_original_method_sig(method_call)
+        unbound_original_method = get_unbound_original_method(method_call)
+        T::Utils.signature_for_method(unbound_original_method)
+      end
+
+      def get_unbound_original_method(method_call)
+        method_name = method_call.method_name
+        mocked_obj = method_call.mocked_obj
+        is_a_class = mocked_obj.is_a? Class
+
+        if is_a_class
+          method_call.mocked_obj.method(method_name)
         else
           method_call.receiver_class.instance_method(method_name)
         end
-        original_method_sig = T::Utils.signature_for_method(unbound_original_method)
+      end
 
-        unless original_method_sig
-          raise MissingSignature, RAISE_ON_MISSING_MESSAGE if raise_on_missing
-          return
-        end
-        block = method_call.block
+      def sig_is_missing(raise_on_missing)
+        raise MissingSignature, RAISE_ON_MISSING_MESSAGE if raise_on_missing
+      end
 
-        mocked_obj.define_singleton_method(method_name) { |*args, &block| method_call.return_value }
-
-        T::Private::Methods::CallValidation.validate_call(
-          mocked_obj,
-          unbound_mocked_method,
-          original_method_sig,
-          args,
-          block
-        )
+      def override_mocked_method_to_avoid_recursion(method_call)
+        method_name = method_call.method_name
+        mocked_obj = method_call.mocked_obj
+        return_value = method_call.return_value
+        mocked_obj.define_singleton_method(method_name) { |*args, &block| return_value }
       end
     end
   end
