d1fe5106e1778b29bcf4436bfc9775f3210d56a1
Vitalii Iurev <vitalii@iurev.space>
2023-03-17 19:03:27 +0300

    fix sorbet call_validation_error_handler

--- lib/mock_suey/sorbet_rspec.rb
+++ lib/mock_suey/sorbet_rspec.rb
@@ -2,12 +2,15 @@
 
 require "sorbet-runtime"
 
+error_handler = T::Configuration.instance_variable_get(:@call_validation_error_handler)
+error_handler ||= proc do |signature, opts|
+  return T::Configuration.send(:call_validation_error_handler_default, signature, opts)
+end
+
 # Let methods with sig receive double/instance_double arguments
 T::Configuration.call_validation_error_handler = lambda do |signature, opts|
   is_mocked = opts[:value].is_a?(RSpec::Mocks::Double) || opts[:value].is_a?(RSpec::Mocks::VerifyingDouble)
-  unless is_mocked
-    return T::Configuration.send(:call_validation_error_handler_default, signature, opts)
-  end
+  return error_handler.call(signature, opts) unless is_mocked
 
   # https://github.com/rspec/rspec-mocks/blob/main/lib/rspec/mocks/verifying_double.rb
   # https://github.com/rspec/rspec-mocks/blob/v3.12.3/lib/rspec/mocks/test_double.rb
@@ -20,5 +23,5 @@ T::Configuration.call_validation_error_handler = lambda do |signature, opts|
   are_related = doubled_class <= opts[:type].raw_type
   return if are_related
 
-  return T::Configuration.send(:call_validation_error_handler_default, signature, opts)
+  error_handler.call(signature, opts)
 end
